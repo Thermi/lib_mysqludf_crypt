24f3efa60477634443f0ed94a62cbf0433dffdd6
Noel Kuntze <noel@familie-kuntze.de>
2019-05-24 02:14:24 +0200

    Add missing DROP FUNCTION in SQL scripts

--- sql/installdb.sql
+++ sql/installdb.sql
@@ -1,6 +1,7 @@
 USE mysql;
-
+START TRANSACTION;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_info;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha1;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha256;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha384;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha512;
@@ -8,7 +9,10 @@ DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha3;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_blake2;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_argon2;
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_scrypt;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_constant_time_compare;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_random;
 
+CREATE FUNCTION lib_mysqludf_crypt_sha1 RETURNS STRING SONAME 'lib_mysqludf_crypt.so';
 CREATE FUNCTION lib_mysqludf_crypt_sha256 RETURNS STRING SONAME 'lib_mysqludf_crypt.so';
 CREATE FUNCTION lib_mysqludf_crypt_sha384 RETURNS STRING SONAME 'lib_mysqludf_crypt.so';
 CREATE FUNCTION lib_mysqludf_crypt_sha512 RETURNS STRING SONAME 'lib_mysqludf_crypt.so';
@@ -18,3 +22,4 @@ CREATE FUNCTION lib_mysqludf_crypt_argon2 RETURNS STRING SONAME 'lib_mysqludf_cr
 CREATE FUNCTION lib_mysqludf_crypt_scrypt RETURNS STRING SONAME 'lib_mysqludf_crypt.so';
 CREATE FUNCTION lib_mysqludf_crypt_constant_time_compare RETURNS INT SONAME 'lib_mysqludf_crypt.so';
 CREATE FUNCTION lib_mysqludf_crypt_random RETURNS INT SONAME 'lib_mysqludf_crypt.so';
+COMMIT;
--- sql/uninstalldb.sql
+++ sql/uninstalldb.sql
@@ -1,10 +1,13 @@
 USE mysql;
 
 DROP FUNCTION IF EXISTS lib_mysqludf_crypt_info;
-DROP FUNCTION IF EXISTS sha256;
-DROP FUNCTION IF EXISTS sha384;
-DROP FUNCTION IF EXISTS sha512;
-DROP FUNCTION IF EXISTS sha3;
-DROP FUNCTION IF EXISTS blake2;
-DROP FUNCTION IF EXISTS argon2;
-DROP FUNCTION IF EXISTS scrypt;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha1;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha256;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha384;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha512;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_sha3;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_blake2;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_argon2;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_scrypt;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_constant_time_compare;
+DROP FUNCTION IF EXISTS lib_mysqludf_crypt_random;
